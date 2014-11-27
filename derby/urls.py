f2a8b3a42e5ffc6e8332cf9d3115c1acdcc5fb07
Deployer na VM serveru <michal@plovarna.cz>
2014-11-27 08:11:59 +0100

    Servirovani statiky jen v debug rezimu

--- derby/urls.py
+++ derby/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
     (r'^odhlaseni/$', 'django.contrib.auth.views.logout', {'next_page': '/index.html'}),
     url(r'^admin/', include(admin.site.urls)),
 )
-urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
